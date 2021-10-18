0dd0174a55cd8acb8bae943eab4a3da7be3fcde0
jihen1998 <57370028+jihen1998@users.noreply.github.com>
2021-10-18 14:39:00 +0100

    reformat code

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../Services/UserService/user.service';
 import {CookieService} from 'ngx-cookie-service';
 import {MenuController} from '@ionic/angular';
-import { Router } from '@angular/router';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { ConfirmationBannerComponent } from '../confirmation-banner/confirmation-banner.component';
+import {Router} from '@angular/router';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {ConfirmationBannerComponent} from '../confirmation-banner/confirmation-banner.component';
 import {AuthService} from '../../Services/Auth/auth.service';
+
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
+
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Tableau de bord',  icon: 'design_app', class: '' },
-    { path: '/commandes-en-atente', title: 'commandes en attente',  icon: 'notifications_active', class: '' },
-    { path: '/commandes-en-cours', title: 'commandes en cours',  icon: 'slow_motion_video', class: '' },
-    { path: '/commandes-termine', title: 'commandes termine',  icon: 'done_all', class: '' },
-    { path: '/commandes-archive', title: 'commandes archive',  icon: 'archive', class: '' },
-    { path: '/ingredient', title: 'ingredient',  icon: 'design_bullet-list-67', class: '' },
-    { path: '/modificateur', title: 'modificateur',  icon: 'text_caps-small', class: '' },
-    { path: '/plat', title: 'plat',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/categorie', title: 'categorie',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/restaurant', title: 'informations du restaurant',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/restaurantmanagement', title: 'informations du restaurant',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/worktime', title: 'temps de travail',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/reduction', title: 'gérer les reductions',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/schedle', title: 'gérer le temps de travaille et les jours fériés',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/offres', title: 'gérer le offres',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/statistics', title: 'Statistiques',  icon: 'objects_spaceship', class: 'active active-pro' },
-    { path: '/printerconfig', title: 'Configuration impriments',  icon: 'objects_spaceship', class: 'active active-pro' }
+  {path: '/dashboard', title: 'Tableau de bord', icon: 'design_app', class: ''},
+  {path: '/commandes-en-atente', title: 'commandes en attente', icon: 'notifications_active', class: ''},
+  {path: '/commandes-en-cours', title: 'commandes en cours', icon: 'slow_motion_video', class: ''},
+  {path: '/commandes-termine', title: 'commandes termine', icon: 'done_all', class: ''},
+  {path: '/commandes-archive', title: 'commandes archive', icon: 'archive', class: ''},
+  {path: '/ingredient', title: 'ingredient', icon: 'design_bullet-list-67', class: ''},
+  {path: '/modificateur', title: 'modificateur', icon: 'text_caps-small', class: ''},
+  {path: '/plat', title: 'plat', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/categorie', title: 'categorie', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/restaurant', title: 'informations du restaurant', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/restaurantmanagement', title: 'informations du restaurant', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/worktime', title: 'temps de travail', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/reduction', title: 'gérer les reductions', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/schedle', title: 'gérer le temps de travaille et les jours fériés', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/offres', title: 'gérer le offres', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/statistics', title: 'Statistiques', icon: 'objects_spaceship', class: 'active active-pro'},
+  {path: '/printerconfig', title: 'Configuration impriments', icon: 'objects_spaceship', class: 'active active-pro'}
 ];
 
 @Component({
@@ -46,26 +48,31 @@ export class SidebarComponent implements OnInit {
   private sidebarVisible: boolean;
 
   constructor(private us: UserService, private router: Router,
-     private auth: AuthService, private cookie: CookieService,
-     private dialog: MatDialog) {    }
-  index_menu=[1,2,3,4,5,6,7,8];
+              private auth: AuthService, private cookie: CookieService,
+              private dialog: MatDialog) {
+  }
+
+  index_menu = [1, 2, 3, 4, 5, 6, 7, 8];
+
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
     if (this.cookie.get('msdigital')) {
       this.msdigitalTest = true;
     }
   }
+
   isMobileMenu() {
-      if ( window.innerWidth > 991) {
-          return false;
-      }
-      return true;
+    if (window.innerWidth > 991) {
+      return false;
+    }
+    return true;
   }
+
   logout() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = false;
-    dialogConfig.width = "15%";
-    dialogConfig.height = "20%";
+    dialogConfig.width = '15%';
+    dialogConfig.height = '20%';
     const dialogref = this.dialog.open(ConfirmationBannerComponent, dialogConfig);
     dialogref.afterClosed().subscribe((retour: boolean) => {
       if (retour === true) {
--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,10 +1,10 @@
 import {filter} from 'rxjs/operators';
 import {Component, OnInit, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
-import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
+import {Location, LocationStrategy, PathLocationStrategy, PopStateEvent} from '@angular/common';
 
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
-import { Subscription ,  Observable } from 'rxjs';
+import {NavbarComponent} from '../../components/navbar/navbar.component';
+import {Router, NavigationEnd, NavigationStart} from '@angular/router';
+import {Subscription, Observable} from 'rxjs';
 import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
@@ -17,8 +17,11 @@ export class AdminLayoutComponent implements OnInit {
   private lastPoppedUrl: string;
   private yScrollStack: number[] = [];
 
-  constructor( public location: Location, private router: Router,private element: ElementRef) {  this.location = location;
-    this.sidebarVisible = false;}
+  constructor(public location: Location, private router: Router, private element: ElementRef) {
+    this.location = location;
+    this.sidebarVisible = false;
+  }
+
   menuItems: any[];
   panelOpenState = false;
   msdigitalTest = false;
@@ -26,42 +29,45 @@ export class AdminLayoutComponent implements OnInit {
   private toggleButton: any;
   public isCollapsed = true;
   private sidebarVisible: boolean;
+
   ngOnInit() {
-      const isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+    const isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
-      if (isWindows && !document.getElementsByTagName('body')[0].classList.contains('sidebar-mini')) {
-          // if we are on windows OS we activate the perfectScrollbar function
+    if (isWindows && !document.getElementsByTagName('body')[0].classList.contains('sidebar-mini')) {
+      // if we are on windows OS we activate the perfectScrollbar function
 
-          document.getElementsByTagName('body')[0].classList.add('perfect-scrollbar-on');
-      } else {
-          document.getElementsByTagName('body')[0].classList.remove('perfect-scrollbar-off');
+      document.getElementsByTagName('body')[0].classList.add('perfect-scrollbar-on');
+    } else {
+      document.getElementsByTagName('body')[0].classList.remove('perfect-scrollbar-off');
+    }
+    const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+    const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        if (event.url != this.lastPoppedUrl) {
+          this.yScrollStack.push(window.scrollY);
+        }
+      } else if (event instanceof NavigationEnd) {
+        if (event.url == this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else {
+          window.scrollTo(0, 0);
+        }
       }
-      const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
-      const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
-
-      this.location.subscribe((ev:PopStateEvent) => {
-          this.lastPoppedUrl = ev.url;
-      });
-       this.router.events.subscribe((event:any) => {
-          if (event instanceof NavigationStart) {
-             if (event.url != this.lastPoppedUrl)
-                 this.yScrollStack.push(window.scrollY);
-         } else if (event instanceof NavigationEnd) {
-             if (event.url == this.lastPoppedUrl) {
-                 this.lastPoppedUrl = undefined;
-                 window.scrollTo(0, this.yScrollStack.pop());
-             } else
-                 window.scrollTo(0, 0);
-         }
-      });
-      this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
-           elemMainPanel.scrollTop = 0;
-           elemSidebar.scrollTop = 0;
-      });
-      if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-          let ps = new PerfectScrollbar(elemMainPanel);
-          ps = new PerfectScrollbar(elemSidebar);
-      }
+    });
+    this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      elemMainPanel.scrollTop = 0;
+      elemSidebar.scrollTop = 0;
+    });
+    if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+      let ps = new PerfectScrollbar(elemMainPanel);
+      ps = new PerfectScrollbar(elemSidebar);
+    }
     const navbar: HTMLElement = this.element.nativeElement;
     this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
     this.router.events.subscribe((event) => {
@@ -73,19 +79,21 @@ export class AdminLayoutComponent implements OnInit {
       }
     });
   }
+
   ngAfterViewInit() {
-      this.runOnRouteChange();
+    this.runOnRouteChange();
   }
-  isMaps(path){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.slice( 1 );
-      if(path == titlee){
-          return false;
-      }
-      else {
-          return true;
-      }
+
+  isMaps(path) {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    titlee = titlee.slice(1);
+    if (path == titlee) {
+      return false;
+    } else {
+      return true;
+    }
   }
+
   runOnRouteChange(): void {
     if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
       const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
@@ -93,23 +101,24 @@ export class AdminLayoutComponent implements OnInit {
       ps.update();
     }
   }
+
   isMac(): boolean {
-      let bool = false;
-      if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
-          bool = true;
-      }
-      return bool;
+    let bool = false;
+    if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+      bool = true;
+    }
+    return bool;
   }
 
   sidebarOpen() {
     const toggleButton = this.toggleButton;
-    const mainPanel =  <HTMLElement>document.getElementsByClassName('main-panel')[0];
+    const mainPanel = <HTMLElement>document.getElementsByClassName('main-panel')[0];
     const html = document.getElementsByTagName('html')[0];
     if (window.innerWidth < 991) {
       mainPanel.style.position = 'fixed';
     }
 
-    setTimeout(function(){
+    setTimeout(function () {
       toggleButton.classList.add('toggled');
     }, 500);
 
@@ -117,19 +126,21 @@ export class AdminLayoutComponent implements OnInit {
 
     this.sidebarVisible = true;
   };
+
   sidebarClose() {
     const html = document.getElementsByTagName('html')[0];
     this.toggleButton.classList.remove('toggled');
-    const mainPanel =  <HTMLElement>document.getElementsByClassName('main-panel')[0];
+    const mainPanel = <HTMLElement>document.getElementsByClassName('main-panel')[0];
 
     if (window.innerWidth < 991) {
-      setTimeout(function(){
+      setTimeout(function () {
         mainPanel.style.position = '';
       }, 500);
     }
     this.sidebarVisible = false;
     html.classList.remove('nav-open');
   };
+
   sidebarToggle() {
     // const toggleButton = this.toggleButton;
     // const html = document.getElementsByTagName('html')[0];
@@ -148,13 +159,13 @@ export class AdminLayoutComponent implements OnInit {
       if ($layer) {
         $layer.remove();
       }
-      setTimeout(function() {
+      setTimeout(function () {
         $toggle.classList.remove('toggled');
       }, 400);
 
       this.mobile_menu_visible = 0;
     } else {
-      setTimeout(function() {
+      setTimeout(function () {
         $toggle.classList.add('toggled');
       }, 430);
 
@@ -164,19 +175,19 @@ export class AdminLayoutComponent implements OnInit {
 
       if (html.querySelectorAll('.main-panel')) {
         document.getElementsByClassName('main-panel')[0].appendChild($layer);
-      }else if (html.classList.contains('off-canvas-sidebar')) {
+      } else if (html.classList.contains('off-canvas-sidebar')) {
         document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
       }
 
-      setTimeout(function() {
+      setTimeout(function () {
         $layer.classList.add('visible');
       }, 100);
 
-      $layer.onclick = function() { //asign a function
+      $layer.onclick = function () { //asign a function
         html.classList.remove('nav-open');
         this.mobile_menu_visible = 0;
         $layer.classList.remove('visible');
-        setTimeout(function() {
+        setTimeout(function () {
           $layer.remove();
           $toggle.classList.remove('toggled');
         }, 400);
@@ -189,14 +200,14 @@ export class AdminLayoutComponent implements OnInit {
   };
 
 
-  collapse(){
+  collapse() {
     this.isCollapsed = !this.isCollapsed;
     const navbar = document.getElementsByTagName('nav')[0];
     console.log(navbar);
     if (!this.isCollapsed) {
       navbar.classList.remove('navbar-transparent');
       navbar.classList.add('bg-white');
-    }else{
+    } else {
       navbar.classList.add('navbar-transparent');
       navbar.classList.remove('bg-white');
     }
